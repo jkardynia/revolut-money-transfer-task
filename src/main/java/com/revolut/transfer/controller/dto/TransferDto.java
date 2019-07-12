d33521930505db1a98b22204e0d98e0ffba17c92
Jarosław Kardynia <jjarekk@gmail.com>
2019-07-12 16:14:53 +0200

    happy path tests

--- src/main/java/com/revolut/transfer/controller/dto/TransferDto.java
+++ src/main/java/com/revolut/transfer/controller/dto/TransferDto.java
@@ -1,8 +1,12 @@
 package com.revolut.transfer.controller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransferDto {
     String source;
     String destination;
--- src/test/groovy/com/revolut/transfer/AccountMoneyTransferIntegrationTest.groovy
+++ src/test/groovy/com/revolut/transfer/AccountMoneyTransferIntegrationTest.groovy
@@ -1,10 +1,19 @@
 package com.revolut.transfer
 
+import com.jayway.restassured.RestAssured
+import com.jayway.restassured.filter.log.RequestLoggingFilter
+import com.jayway.restassured.http.ContentType
+import com.jayway.restassured.response.Response
 import com.revolut.Application
+import com.revolut.transfer.controller.dto.TransferDto
 import org.junit.AfterClass
 import org.junit.BeforeClass
 import spock.lang.Shared
 import spock.lang.Specification
+
+import static com.jayway.restassured.RestAssured.get
+import static com.jayway.restassured.RestAssured.given
+import static org.hamcrest.CoreMatchers.equalTo
 
 class AccountMoneyTransferIntegrationTest extends Specification{
 
@@ -13,7 +22,11 @@ class AccountMoneyTransferIntegrationTest extends Specification{
 
     @BeforeClass
     def startApplication(){
-        app = new Application(8080)
+
+        def appPort = 8080
+        app = new Application(appPort)
+        RestAssured.baseURI = "http://localhost"
+        RestAssured.port = appPort
     }
 
     @AfterClass
@@ -21,16 +34,52 @@ class AccountMoneyTransferIntegrationTest extends Specification{
         if(app != null) {
             app.stop()
         }
+        RestAssured.reset()
     }
 
-    def "hello world test"(){
+    def "get account details"(){
         given:
-        def a = "string 1"
+        // I am using existing accounts to make it simpler for sake of this exercise
+        // not to hit DB directly or add account via API
 
         when:
-        def b = "string 1"
+        Response response = get("/account/123456")
 
         then:
-        a == b
+        response.then().assertThat()
+            .body("balance", equalTo(100))
+            .body("number", equalTo("123456"))
+            .body("owner", equalTo("Ryszard Kowalski"))
+            .statusCode(200)
+    }
+
+    def "transfer money"(){
+        given:
+        // I am using existing accounts to make it simpler for sake of this exercise
+        // not to hit DB directly or add account via API
+
+        def from = "12345"
+        def to = "123456"
+        def amount = 50L
+
+        def request = given()
+                .body(new TransferDto(from, to, amount))
+        .contentType(ContentType.JSON)
+                .filter(new RequestLoggingFilter())
+
+        when:
+        Response response = request.post("/transfer")
+
+        then:
+        response.then().assertThat()
+                .statusCode(204)
+
+        get("/account/123456")
+                .then().assertThat()
+                .body("balance", equalTo(150))
+
+        get("/account/12345")
+                .then().assertThat()
+                .body("balance", equalTo(50))
     }
 }
